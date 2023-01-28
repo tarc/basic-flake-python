9c65b73c8da37ad86f1e987bde5cd4881b5c1860
Tarcísio Genaro Rodrigues <silvia@MacBook-Air-de-Silvia.local>
2023-01-28 16:39:44 -0300

    Fix versions to get it working

---
+++ README.md
@@ -0,0 +1,26 @@
+# Build custom python environment
+
+## `mach-nix`
+
+- Github repository: [DavHau/mach-nix][githubMatchNix];
+- Git commit: [`913e6c16f986746ba5507878ef7ff992804d1fa8`][matchNixRef];
+- On: Nov 8, 2022;
+- Last commit on `master` branch before project goes officially unmaintained.
+
+[githubMatchNix]: https://github.com/DavHau/mach-nix
+[matchNixRef]: https://github.com/DavHau/mach-nix/commits/913e6c16f986746ba5507878ef7ff992804d1fa8
+
+## `pypi-deps-db`
+
+- Github repository: [DavHau/pypi-deps-db][githubPypiDepsDb];
+- Git commit: [`91adfffe522d3571e6549c77695adaa5ef3fecc5`][pypiDepsDbRef]:
+- On: Jan 12, 2023.
+
+[githubPypiDepsDb]: https://github.com/DavHau/pypi-deps-db
+[pypiDepsDbRef]: https://github.com/DavHau/pypi-deps-db/commits/91adfffe522d3571e6549c77695adaa5ef3fecc5
+
+## Start shell
+
+```bash
+nix -v --accept-flake-config --show-trace develop
+```
--- flake.nix
+++ flake.nix
@@ -1,42 +1,56 @@
 {
   description = "A very basic flake";
 
+  nixConfig = {
+    substituters = ["https://cache.nixos.org" "https://kclejeune.cachix.org" "https://tarc.cachix.org"];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "kclejeune.cachix.org-1:fOCrECygdFZKbMxHClhiTS6oowOkJ/I/dh9q9b1I4ko="
+      "tarc.cachix.org-1:wIYVNrWvfOFESyas4plhMmGv91TjiTBVWB0oqf1fHcE="
+    ];
+
+    extra-experimental-features = "nix-command flakes";
+  };
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/22.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     # This section will allow us to create a python environment
     # with specific predefined python packages from PyPi
-    pypi-deps-db = {
-      url = "github:DavHau/pypi-deps-db";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.mach-nix.follows = "mach-nix";
-    };
     mach-nix = {
-      url = "github:DavHau/mach-nix/3.5.0";
+      url = "mach-nix/913e6c16f986746ba5507878ef7ff992804d1fa8";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pypi-deps-db.follows = "pypi-deps-db";
+
+    };
+    pypi-deps-db = {
+      url = "github:DavHau/pypi-deps-db?rev=91adfffe522d3571e6549c77695adaa5ef3fecc5";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.mach-nix.follows = "mach-nix";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, mach-nix, ... }@attr:
+  outputs = { self, nixpkgs, flake-utils, mach-nix, pypi-deps-db, ... }@attr:
   flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+      mach-nix_ = (import mach-nix) {
+        inherit pkgs;
+        pypiDataRev = pypi-deps-db.rev;
+        pypiDataSha256 = pypi-deps-db.narHash;
+        python = "python310";
+      };
 
       # create a custom python environment
-      myPython = mach-nix.lib.${system}.mkPython {
-        # specify the base version of python you with to use
-        python = "python310";
-
+      myPython = mach-nix_.mkPython ({
+        # With custom packages
         requirements = ''
-          numpy
-          Sphinx
-          sphinx-rtd-theme
-          sphinx-autodoc-annotation
+          conan
         '';
-      };
+      });
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = [
